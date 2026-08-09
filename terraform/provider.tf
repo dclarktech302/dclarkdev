462b6a6421f1992edf2732868570d8fc499d07da
dclarktech302 <dcps302@gmail.com>
2026-08-09 13:17:26 -0400

    unique key for tf state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,7 +6,7 @@ terraform {
     backend "s3" {
         bucket = "tf-resources-github-actions"
         region = "us-east-1"
-        key = "github-actions/terraform.tfstate"
+        key = "dclarkdev-tfstate/terraform.tfstate"
         encrypt = true
         dynamodb_table = "tf-resources-github-actions"
     }
