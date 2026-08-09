10ee0d0ea17580881f7464dfb7dabb36865f7f12
dclarktech302 <dcps302@gmail.com>
2026-08-09 13:03:53 -0400

    rename variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,30 @@
-resource "aws_s3_bucket" "static_site" {
+resource "aws_s3_bucket" "dclarkdev-static" {
   bucket = var.bucket_name
 }
 
 # moved {
-#   from = aws_s3_bucket.static-site
-#   to   = aws_s3_bucket.static_site
+#   from = aws_s3_bucket.dclarkdev-static
+#   to   = aws_s3_bucket.dclarkdev-static
 # }
 
-# resource "aws_s3_bucket_website_configuration" "static-site-config" {
-#   bucket = aws_s3_bucket.static_site.id
+# resource "aws_s3_bucket_website_configuration" "dclarkdev-static-config" {
+#   bucket = aws_s3_bucket.dclarkdev-static.id
 
 #   index_document {
 #     suffix = "index.html"
 #   }
 # }
 
-resource "aws_s3_bucket_public_access_block" "static_site_access" {
-  bucket = aws_s3_bucket.static_site.id
+resource "aws_s3_bucket_public_access_block" "dclarkdev-static_access" {
+  bucket = aws_s3_bucket.dclarkdev-static.id
   block_public_acls = true
   block_public_policy = true
   ignore_public_acls = true
   restrict_public_buckets = true
 }
 
-# resource "aws_s3_bucket_policy" "static_site_policy" {
-#   bucket = aws_s3_bucket.static_site.id
+# resource "aws_s3_bucket_policy" "dclarkdev-static_policy" {
+#   bucket = aws_s3_bucket.dclarkdev-static.id
 
 #   policy = jsonencode({
 #     Version = "2012-10-17"
@@ -33,10 +33,10 @@ resource "aws_s3_bucket_public_access_block" "static_site_access" {
 #         Effect = "Allow"
 #         Principal = "*"
 #         Action = "s3:GetObject"
-#         Resource = "${aws_s3_bucket.static_site.arn}/*"
+#         Resource = "${aws_s3_bucket.dclarkdev-static.arn}/*"
 #       }
 #     ]
 #   })
 
-#   depends_on = [ aws_s3_bucket_public_access_block.static_site_access ]
+#   depends_on = [ aws_s3_bucket_public_access_block.dclarkdev-static_access ]
 # }
