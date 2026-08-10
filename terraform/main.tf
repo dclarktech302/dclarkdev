e455c8a9b43c33fcb30ed1baf7e3de9ff9fd257c
dclarktech302 <dcps302@gmail.com>
2026-08-10 12:00:55 -0400

    validate ssl and get data with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,3 +55,30 @@ resource "aws_acm_certificate" "dclarkdev" {
     create_before_destroy = true
   }
 }
+
+data "aws_route_53_zone" "domain_zone" {
+  name = "dclarkdev.com"
+}
+
+resource "aws_route53_record" "dclarkdev_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.dclarkdev_cert_validation.domain_validation_options : dvo.domain_name
+    =>{
+      name = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type = dvo.resource_record_type
+    }
+  }
+
+  name = each.value.name
+  records = [each.value.record]
+  ttl = 60
+  type = each.value.type
+  zone_id = data.aws_route_53_zone.domain_zone.zone_id
+}
+
+resource "aws_acm_certificate" "dclarkdev_cert_validation" {
+  certificate_arn = aws_aws_certificate.dclarkdev_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.dclarkdev_cert_validation : record.fqdn]
+}
+
