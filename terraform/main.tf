b60936087319f3acd97db72c432c22df5d8d1663
dclarktech302 <dcps302@gmail.com>
2026-08-10 11:27:16 -0400

    simplfy gha files, make s3 bucket private

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,36 +7,36 @@ resource "aws_s3_bucket" "dclarkdev-static" {
 #   to   = aws_s3_bucket.dclarkdev-static
 # }
 
-resource "aws_s3_bucket_website_configuration" "dclarkdev-static-config" {
-  bucket = aws_s3_bucket.dclarkdev-static.id
+# resource "aws_s3_bucket_website_configuration" "dclarkdev-static-config" {
+#   bucket = aws_s3_bucket.dclarkdev-static.id
 
-  index_document {
-    suffix = "index.html"
-  }
-}
+#   index_document {
+#     suffix = "index.html"
+#   }
+# }
 
-resource "aws_s3_bucket_public_access_block" "dclarkdev-static_access" {
-  bucket = aws_s3_bucket.dclarkdev-static.id
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
-  restrict_public_buckets = false
-}
+# resource "aws_s3_bucket_public_access_block" "dclarkdev-static_access" {
+#   bucket = aws_s3_bucket.dclarkdev-static.id
+#   block_public_acls = true
+#   block_public_policy = true
+#   ignore_public_acls = true
+#   restrict_public_buckets = true
+# }
 
-resource "aws_s3_bucket_policy" "dclarkdev-static_policy" {
-  bucket = aws_s3_bucket.dclarkdev-static.id
+# resource "aws_s3_bucket_policy" "dclarkdev-static_policy" {
+#   bucket = aws_s3_bucket.dclarkdev-static.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = "*"
-        Action = "s3:GetObject"
-        Resource = "${aws_s3_bucket.dclarkdev-static.arn}/*"
-      }
-    ]
-  })
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = "*"
+#         Action = "s3:GetObject"
+#         Resource = "${aws_s3_bucket.dclarkdev-static.arn}/*"
+#       }
+#     ]
+#   })
 
-  depends_on = [ aws_s3_bucket_public_access_block.dclarkdev-static_access ]
-}
+#   depends_on = [ aws_s3_bucket_public_access_block.dclarkdev-static_access ]
+# }
